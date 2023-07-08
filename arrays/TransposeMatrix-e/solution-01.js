4f567f7b6fab5b024ec908a8f560abbca5aab6d8
Juan31416 <j.pabloesza98@gmail.com>
2023-07-09 01:58:24 +0900

    feat: add transposed matrix solution

---
+++ arrays/TransposeMatrix-e/solution-01.js
@@ -0,0 +1,41 @@
+/** 
+ * Transpose Matrix
+ * input: array of arrays
+ * output: array of arrays
+ * assumptions: the input will always have at least one input
+ * */ 
+
+function transposeMatrix(matrix){
+  const newYsize = matrix[0].length;
+  const newXsize = matrix.length;
+ 
+  if(newXSize <= 1 && newYSize <= 1) return matrix;
+  
+  const newTransposedMatrix = [...Array(newYsize)].map(e => Array(newXSize));
+
+  matrix.forEach((rowElement, yIndex) => {
+    rowElement.forEach((individualElement, xIndex) => {
+      newTransposedMatrix[xIndex][yIndex] = individualElement;
+    })
+    
+  })
+
+  return newTransposedMatrix;
+}
+
+// Do not edit the line bellow.
+export.transposeMatrix = transposeMatrix;
+
+/** 
+ * Solution 1
+ * Switching Axis Approach
+ * Time: O(w*h)
+ * Space: O(w*h)
+ * Check length size of both matrix
+ * If it is smaller than 1 return the Matrix
+ * else create a new empty one with the inverse quantiy of 
+ * space, then traverse the original and switch the x and y 
+ * elements for each element.
+ * */ 
+
+
