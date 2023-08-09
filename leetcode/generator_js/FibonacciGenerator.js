c091318ee6f51bbf20a0a422c2fb365436c79e22
Juan31416 <j.pabloesza98@gmail.com>
2023-08-09 17:42:17 +0900

    feat: add only once function execution solution

--- leetcode/generator_js/FibonacciGenerator.js
+++ leetcode/generator_js/FibonacciGenerator.js
@@ -1,1 +1,16 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function* () {
+  let [prev, curr] = [0, 1];
+  while (true) {
+    yield prev;
+    [prev, curr] = [curr, prev + curr];
+  }
+};
 
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
