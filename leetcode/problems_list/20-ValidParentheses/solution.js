a8adb013a70f6b35fb17c843a4f4c34e77659999
Juan31416 <j.pabloesza98@gmail.com>
2024-05-10 10:00:50 -0600

    feat: add second solution using map and stack

--- leetcode/problems_list/20-ValidParentheses/solution.js
+++ leetcode/problems_list/20-ValidParentheses/solution.js
@@ -2,7 +2,41 @@
  * @param {string} s
  * @return {boolean}
  */
+
+// Solution 2: Using stack & checking each paretheses character
+// Time complexity is O(n) where n is the length of the string
+// Space complexity is O(n) where n is the length of the string
+
+var isValid = function (s) {
+  if (s.length % 2 !== 0) return false;
+
+  const pMap = { ")": "(", "]": "[", "}": "{" };
+  let checkStack = [];
+
+  for (i in s) {
+    if (s[i] in pMap) {
+      if (
+        checkStack.length > 0 &&
+        checkStack[checkStack.length - 1] === pMap[s[i]]
+      ) {
+        checkStack.pop();
+      } else {
+        return false;
+      }
+    } else {
+      checkStack.push(s[i]);
+    }
+  }
+
+  return checkStack.length === 0;
+};
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ 
 // Solution 1: Using stack & checking each paretheses character
+
 // Time complexity is O(n) where n is the length of the string
 // Space complexity is O(n) where n is the length of the string
 var isValid = function (s) {
@@ -49,3 +83,4 @@ var isValid = function (s) {
 
   return validation.every((curr) => curr === 0);
 };
+*/
