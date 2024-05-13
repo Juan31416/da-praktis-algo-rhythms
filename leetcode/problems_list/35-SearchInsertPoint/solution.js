e608ede58fba3c2c31760d3b39d07d8500259367
Juan31416 <j.pabloesza98@gmail.com>
2024-05-13 10:06:59 -0600

    feat: delete comments

--- leetcode/problems_list/35-SearchInsertPoint/solution.js
+++ leetcode/problems_list/35-SearchInsertPoint/solution.js
@@ -10,10 +10,8 @@ var searchInsert = function (nums, target) {
   let targetDiff = nums[nums.length - 1] * 10;
   let position = 0;
   for (const [idx, curr] of nums.entries()) {
-    console.log(Math.abs(curr - target) < targetDiff, curr - target);
     if (Math.abs(curr - target) < targetDiff) {
       targetDiff = Math.abs(curr - target);
-      console.log("inner", targetDiff, curr - target, position, idx);
       if (targetDiff === 0 || curr - target > 0) position = idx;
       if (curr - target < 0) position = idx + 1;
     }
