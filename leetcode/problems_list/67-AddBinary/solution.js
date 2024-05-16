1a4aedfc00a8ee72f206b7b290f394907e62613d
Juan31416 <j.pabloesza98@gmail.com>
2024-05-16 10:09:11 -0600

    feat: add simplified solution for leetcode problem 67

--- leetcode/problems_list/67-AddBinary/solution.js
+++ leetcode/problems_list/67-AddBinary/solution.js
@@ -3,31 +3,50 @@
  * @param {string} b
  * @return {string}
  */
-
-// Solution 1
+// Solution 2
 // Time Complexity: O(n)
-// Space Complexity: O(n)
+// Space Complexity: O(1)
 
 var addBinary = function (a, b) {
-  let result = [];
-  let remainder = 0;
-  let shortest = (a.length < b.length ? [...a] : [...b]).reverse();
-  let longest = (a.length < b.length ? [...b] : [...a]).reverse();
+  const lenA = a.length - 1;
+  const lenB = b.length - 1;
 
-  for (let i = 0; i < longest.length; i++) {
-    let sum = 0;
-    if (shortest[i]) {
-      sum = +shortest[i] + +longest[i] + remainder;
-      result.push(sum % 2);
-      remainder = sum > 1 ? 1 : 0;
-    } else {
-      sum = +longest[i] + remainder;
-      result.push(sum % 2);
-      remainder = sum === 2 ? 1 : 0;
-    }
+  let carry = 0;
+  let result = "";
+
+  for (let i = lenA, j = lenB; i >= 0 || j >= 0; i--, j--) {
+    let sum = (Number(a[i]) || 0) + (Number(b[j]) || 0) + carry;
+    result = (sum % 2) + result;
+    carry = sum > 1 ? 1 : 0;
   }
 
-  if (remainder === 1) result.push(1);
-
-  return result.reverse().join("");
+  return carry === 1 ? "1" + result : result;
 };
+
+// // Solution 1
+// // Time Complexity: O(n)
+// // Space Complexity: O(n)
+
+// var addBinary = function (a, b) {
+//   let result = [];
+//   let remainder = 0;
+//   let shortest = (a.length < b.length ? [...a] : [...b]).reverse();
+//   let longest = (a.length < b.length ? [...b] : [...a]).reverse();
+
+//   for (let i = 0; i < longest.length; i++) {
+//     let sum = 0;
+//     if (shortest[i]) {
+//       sum = +shortest[i] + +longest[i] + remainder;
+//       result.push(sum % 2);
+//       remainder = sum > 1 ? 1 : 0;
+//     } else {
+//       sum = +longest[i] + remainder;
+//       result.push(sum % 2);
+//       remainder = sum > 1 ? 1 : 0;
+//     }
+//   }
+
+//   if (remainder === 1) result.push(1);
+
+//   return result.reverse().join("");
+// };
