2d1c8fde7e4af2c7921f934e6c8ca8b11f014560
Juan31416 <j.pabloesza98@gmail.com>
2024-05-17 10:02:49 -0600

    feat: add more efficient solution and intro to binary search

--- leetcode/problems_list/69-SqrtX/solution.js
+++ leetcode/problems_list/69-SqrtX/solution.js
@@ -3,18 +3,38 @@
  * @return {number}
  */
 
-// Solution 1
-// time complexity: O(sqrt(n))
-// space complexity: O(1)
+// Solution 2
+// Time complexity: O(log(n))
+// Space complexity: O(1)
 
 var mySqrt = function (x) {
-  let i = 1;
-  while (i * i <= x) {
-    if ((i * i) % x == 0) {
-      return i;
-    }
-    i += 1;
-  }
+  let left = 0,
+    right = x;
 
-  return i - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (mid * mid <= x && (mid + 1) * (mid + 1) > x) {
+      return mid;
+    } else if (mid * mid < x) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
 };
+
+// // Solution 1
+// // time complexity: O(sqrt(n))
+// // space complexity: O(1)
+
+// var mySqrt = function (x) {
+//   let i = 1;
+//   while (i * i <= x) {
+//     if ((i * i) % x == 0) {
+//       return i;
+//     }
+//     i += 1;
+//   }
+
+//   return i - 1;
+// };
