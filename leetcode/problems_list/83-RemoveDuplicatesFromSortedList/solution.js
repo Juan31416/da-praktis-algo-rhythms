931064f5b8f474a62190922e2e53f1f571f0b6f9
Juan31416 <j.pabloesza98@gmail.com>
2024-05-19 10:10:37 -0600

    feat: add second solution to leetcode problem 83

--- leetcode/problems_list/83-RemoveDuplicatesFromSortedList/solution.js
+++ leetcode/problems_list/83-RemoveDuplicatesFromSortedList/solution.js
@@ -10,21 +10,55 @@
  * @return {ListNode}
  */
 
-// Solution 1
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+// Solution 2 - My solution
 // Time Complexity: O(n)
-// Space Complexity: O(n)
+// Space Complexity: O(1)
 
 var deleteDuplicates = function (head) {
-  if (head == null) {
-    return head;
-  }
-  let node = head;
-  while (node.next) {
-    if (node.val == node.next.val) {
-      node.next = node.next.next;
+  if (head === null || head.next === null) return head;
+
+  let left = head;
+  let right = head.next;
+
+  while (left.next !== null) {
+    if (left.val === right.val) {
+      left.next = right.next ?? null;
+      right = right.next ?? null;
     } else {
-      node = node.next;
+      left = right;
+      right = right.next;
     }
   }
   return head;
 };
+
+// // Solution 1 - Not my solution Only one pointer
+// // Time Complexity: O(n)
+// // Space Complexity: O(1)
+
+// var deleteDuplicates = function (head) {
+//   if (head == null) {
+//     return head;
+//   }
+//   let node = head;
+//   while (node.next) {
+//     if (node.val == node.next.val) {
+//       node.next = node.next.next;
+//     } else {
+//       node = node.next;
+//     }
+//   }
+//   return head;
+// };
