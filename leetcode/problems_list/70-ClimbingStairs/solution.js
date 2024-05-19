6999a0dd515384716ec5736a2fbd8418a67d5e3f
Juan31416 <j.pabloesza98@gmail.com>
2024-05-19 09:37:06 -0600

    feat: add iterative solution for leetcode problem 70

--- leetcode/problems_list/70-ClimbingStairs/solution.js
+++ leetcode/problems_list/70-ClimbingStairs/solution.js
@@ -2,16 +2,22 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
-    if(n === 1 ) return 1
 
-    let result = n%2 == 0 ? 2 : 1;
+// Solution 1: Iterative Fibonacci
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
-    const repetitions = n%2 == 0 ? Math.floor(n/2) - 1 : Math.floor(n/2); 
+var climbStairs = function (n) {
+  if (n == 1) return 1;
 
-    for(let i = 1; i <= repetitions; i++){
-      result += (((n - i*2)/2) * ((n - i*2)/1)) + 1
-    }
+  let prev = 1;
+  let next = 1;
 
-    return result;
+  for (let i = 0; i < n - 1; i++) {
+    let temp = prev;
+    prev = next;
+    next += temp;
+  }
+
+  return next;
 };
