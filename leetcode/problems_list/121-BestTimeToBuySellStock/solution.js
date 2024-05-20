4832578d2db89336c832082644724db17d688e9b
Juan31416 <j.pabloesza98@gmail.com>
2024-05-20 10:06:23 -0600

    feat: add final solution to leetcode problem 121

--- leetcode/problems_list/121-BestTimeToBuySellStock/solution.js
+++ leetcode/problems_list/121-BestTimeToBuySellStock/solution.js
@@ -3,17 +3,24 @@
  * @return {number}
  */
 
-// WIP
+// Solution 1
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
 var maxProfit = function (prices) {
   if (prices.lenght === 1) return 0;
 
-  let minValue = [Math.min(...prices), prices.indexOf(Math.min(...prices))];
-  let maxValue = minValue[0];
+  let minValue = prices[0];
+  let profit = 0;
 
-  for (let i = minValue[1]; i < prices.length; i++) {
-    if (prices[i] > maxValue) maxValue = prices[i];
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] - minValue > profit) {
+      profit = prices[i] - minValue;
+    }
+    if (prices[i] < minValue) {
+      minValue = prices[i];
+    }
   }
 
-  return maxValue - minValue[0];
+  return profit;
 };
