c80509c6e01611db353131ee440673c6dfc6b799
Juan31416 <j.pabloesza98@gmail.com>
2024-05-22 10:16:43 -0600

    feat: add final solution to leetcode problem 168

--- leetcode/problems_list/168-ExcelSheetColumnTitle/solution.js
+++ leetcode/problems_list/168-ExcelSheetColumnTitle/solution.js
@@ -2,10 +2,23 @@
  * @param {number} columnNumber
  * @return {string}
  */
+
+// Solution 1
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
 var convertToTitle = function (columnNumber) {
   let finalColumn = "";
-
-  while (columnNumber > 26) {
-    finalColumn = String.fromCharCode(columnNumber % 26) + finalColumn;
+  while (columnNumber > 0) {
+    finalColumn =
+      String.fromCharCode(
+        columnNumber % 26 == 0
+          ? (columnNumber % 26) + 90
+          : (columnNumber % 26) + 64
+      ) + finalColumn;
+    columnNumber =
+      (columnNumber - (columnNumber % 26 == 0 ? 26 : columnNumber % 26)) / 26;
   }
+
+  return finalColumn;
 };
