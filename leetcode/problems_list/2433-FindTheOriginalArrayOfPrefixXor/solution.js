b1590a261070c4280c34d0665bea9acb6e977da9
Juan31416 <j.pabloesza98@gmail.com>
2024-06-08 23:57:49 -0600

    feat: add solution to leetcode problem 2433

--- leetcode/problems_list/2433-FindTheOriginalArrayOfPrefixXor/solution.js
+++ leetcode/problems_list/2433-FindTheOriginalArrayOfPrefixXor/solution.js
@@ -3,6 +3,17 @@
  * @return {number[]}
  */
 
+// Solution 2 - In-place
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+var findArray = function (pref) {
+  for (let i = pref.length - 1; i > 0; i--) {
+    pref[i] ^= pref[i - 1];
+  }
+  return pref;
+};
+
 // Solution 1
 // Time Complexity: O(n)
 // Space Complexity: O(n)
