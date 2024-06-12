f17f37282a479a04172a8b7664a1c625d3bb161a
Juan31416 <j.pabloesza98@gmail.com>
2024-06-12 09:28:23 -0600

    feat: add second solution to leetcode problem 1572

--- leetcode/problems_list/1572-MatrixDiagonalSum/solution.js
+++ leetcode/problems_list/1572-MatrixDiagonalSum/solution.js
@@ -2,6 +2,22 @@
  * @param {number[][]} mat
  * @return {number}
  */
+// Solution 2
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+var diagonalSum = function (mat) {
+  let sum = 0;
+  let center = Math.floor(mat.length / 2);
+
+  for (let i = 0; i < mat.length; i++) {
+    sum +=
+      i === center && mat.length % 2 !== 0
+        ? mat[i][i]
+        : mat[i][i] + mat[i][mat.length - 1 - i];
+  }
+
+  return sum;
+};
 
 // Solution 1
 // Time Complexity: O(n)
