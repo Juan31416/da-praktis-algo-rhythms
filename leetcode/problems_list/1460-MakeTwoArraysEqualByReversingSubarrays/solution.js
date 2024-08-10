b9f20b648169e41837bf812b3baf1d0775cc7b47
Juan31416 <j.pabloesza98@gmail.com>
2024-08-09 19:19:14 -0600

    feat: add 3th solution

--- leetcode/problems_list/1460-MakeTwoArraysEqualByReversingSubarrays/solution.js
+++ leetcode/problems_list/1460-MakeTwoArraysEqualByReversingSubarrays/solution.js
@@ -4,6 +4,17 @@
  * @return {boolean}
  */
 
+// Solution 3 - Sort
+// Time Complexity: O(n log n)
+// Space Complexity: O(1)
+
+var canBeEqual = function (target, arr) {
+  return (
+    target.sort((a, b) => a - b).toString() ===
+    arr.sort((a, b) => a - b).toString()
+  );
+};
+
 // Solution 2 - Hash Map
 // Time Complexity: O(n)
 // Space Complexity: O(n)
