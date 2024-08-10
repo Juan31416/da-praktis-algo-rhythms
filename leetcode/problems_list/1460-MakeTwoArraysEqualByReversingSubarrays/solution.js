fc0261992de8b595f511b2d83fa32f032e8cf071
Juan31416 <j.pabloesza98@gmail.com>
2024-08-09 19:18:41 -0600

    feat: add solution to leetcode problem 1460

---
+++ leetcode/problems_list/1460-MakeTwoArraysEqualByReversingSubarrays/solution.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+
+// Solution 2 - Hash Map
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+var canBeEqual = function (target, arr) {
+  const map = new Map();
+
+  for (let i = 0; i < target.length; i++) {
+    map.set(target[i], (map.get(target[i]) || 0) + 1);
+    map.set(arr[i], (map.get(arr[i]) || 0) - 1);
+  }
+
+  return [...map.values()].every((val) => val === 0);
+};
+
+// Solution 1 - Sort
+// Time Complexity: O(n log n)
+// Space Complexity: O(1)
+
+var canBeEqual = function (target, arr) {
+  target.sort((a, b) => a - b);
+  arr.sort((a, b) => a - b);
+
+  return target.every((curr, idx) => curr === arr[idx]);
+};
