56217c19b24511173f5c2e2d11d20c108c99286b
Juan31416 <j.pabloesza98@gmail.com>
2024-08-21 22:22:42 -0600

    feat: add solution to leetcode problem 2357

--- leetcode/problems_list/2357-MakeArrayZeroBySubstractingEqualAmounts/solution.js
+++ leetcode/problems_list/2357-MakeArrayZeroBySubstractingEqualAmounts/solution.js
@@ -3,20 +3,30 @@
  * @return {number}
  */
 
+// Solution 2 - Set
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+var minimumOperations = function (nums) {
+  const set = new Set(nums);
+  set.delete(0);
+  return set.size;
+};
+
 // Solution 1
 // Time Complexity: O(n)
 // Space Complexity: O(1)
-
 var minimumOperations = function (nums) {
-  let moreThanZero = nums.every((curr) => curr > 0);
-  let minVal = Math.min(...nums);
+  let moreThanZero = nums.every((curr) => curr < 0);
+  let minVal = Math.min(...[...nums].filter((curr) => curr > 0));
   let count = 0;
 
-  while (moreThanZero) {
+  while (!moreThanZero) {
     nums = nums.map((curr) => curr - minVal);
-    moreThanZero = nums.every((curr) => curr > 0);
+    moreThanZero = nums.every((curr) => curr < 0);
+    minVal = Math.min(...[...nums].filter((curr) => curr > 0));
     count++;
   }
 
-  return count;
+  return count - 1;
 };
