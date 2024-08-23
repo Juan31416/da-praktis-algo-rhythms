2da405f934fe67d1663304c7c43dd050496c0fb8
Juan31416 <j.pabloesza98@gmail.com>
2024-08-22 23:41:44 -0600

    feat: add initial solution to leetcode problem 867

---
+++ leetcode/problems_list/867-TransposeMatrix/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var transpose = function (matrix) {
+  let newMatrix = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {}
+  }
+};
