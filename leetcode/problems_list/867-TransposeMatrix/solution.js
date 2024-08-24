cba962a1ef4cc6b36ad86ddf5a751cfc2f06819d
Juanπ <j.pabloesza98@gmail.com>
2024-08-23 22:55:54 -0600

    feat: add solution to leetcode problem 867

--- leetcode/problems_list/867-TransposeMatrix/solution.js
+++ leetcode/problems_list/867-TransposeMatrix/solution.js
@@ -2,10 +2,19 @@
  * @param {number[][]} matrix
  * @return {number[][]}
  */
+// Solution 1 - Transpose matrix
+// Time complexity: O(n^2)
+// Space complexity: O(n^2)
 var transpose = function (matrix) {
   let newMatrix = [];
 
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[0].length; j++) {}
+  for (let i = 0; i < matrix[0].length; i++) {
+    let row = [];
+    for (let j = 0; j < matrix.length; j++) {
+      row.push(matrix[j][i]);
+    }
+    newMatrix.push(row);
   }
+
+  return newMatrix;
 };
